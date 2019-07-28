1fa8ffe6dee57d08da0f1b610a4e0f4b3c929d27
terekhov-aa <12root12@gmail.com>
2019-07-28 12:40:05 +0300

    migrations

---
+++ Db/Migrations/20190728084934_Houses.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Db.Migrations
+{
+    public partial class Houses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "Subnets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Address = table.Column<string>(nullable: true),
+                    Mask = table.Column<int>(nullable: false),
+                    Gateway = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subnets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SystemUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Login = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    isActive = table.Column<bool>(nullable: false),
+                    Role = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SystemUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WhiteAddress",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WhiteAddress", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Houses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Address = table.Column<string>(nullable: true),
+                    Number = table.Column<string>(nullable: true),
+                    AdditionalNumber = table.Column<string>(nullable: true),
+                    Comment = table.Column<string>(nullable: true),
+                    Porch = table.Column<string>(nullable: true),
+                    SubnetId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Houses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Houses_Subnets_SubnetId",
+                        column: x => x.SubnetId,
+                        principalTable: "Subnets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FullAddress",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Grey = table.Column<string>(nullable: true),
+                    WhiteId = table.Column<int>(nullable: true),
+                    SubnetId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FullAddress", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FullAddress_Subnets_SubnetId",
+                        column: x => x.SubnetId,
+                        principalTable: "Subnets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FullAddress_WhiteAddress_WhiteId",
+                        column: x => x.WhiteId,
+                        principalTable: "WhiteAddress",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    Patronymic = table.Column<string>(nullable: true),
+                    Login = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    HwIpAddress = table.Column<string>(nullable: true),
+                    HwPort = table.Column<string>(nullable: true),
+                    Comment = table.Column<string>(nullable: true),
+                    Balance = table.Column<double>(nullable: false),
+                    Status = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    HouseId = table.Column<int>(nullable: false),
+                    FullAddressId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clients_FullAddress_FullAddressId",
+                        column: x => x.FullAddressId,
+                        principalTable: "FullAddress",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Clients_Houses_HouseId",
+                        column: x => x.HouseId,
+                        principalTable: "Houses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_FullAddressId",
+                table: "Clients",
+                column: "FullAddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_HouseId",
+                table: "Clients",
+                column: "HouseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FullAddress_SubnetId",
+                table: "FullAddress",
+                column: "SubnetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FullAddress_WhiteId",
+                table: "FullAddress",
+                column: "WhiteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Houses_SubnetId",
+                table: "Houses",
+                column: "SubnetId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "SystemUsers");
+
+            migrationBuilder.DropTable(
+                name: "FullAddress");
+
+            migrationBuilder.DropTable(
+                name: "Houses");
+
+            migrationBuilder.DropTable(
+                name: "WhiteAddress");
+
+            migrationBuilder.DropTable(
+                name: "Subnets");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Login = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Role = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+    }
+}
