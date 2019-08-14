d908a12ac25e073a83877af2b6a0dfd19ea185f9
terekhov-aa <12root12@gmail.com>
2019-08-14 22:20:37 +0300

    mig

---
+++ Db/Migrations/20190814183745_client.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Db.Migrations
+{
+    public partial class client : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Patronymic",
+                table: "Clients",
+                newName: "MiddleName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MiddleName",
+                table: "Clients",
+                newName: "Patronymic");
+        }
+    }
+}
---
+++ Db/Migrations/20190814190643_client1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Db.Migrations
+{
+    public partial class client1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Houses_Subnets_SubnetId",
+                table: "Houses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubnetId",
+                table: "Houses",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Houses_Subnets_SubnetId",
+                table: "Houses",
+                column: "SubnetId",
+                principalTable: "Subnets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Houses_Subnets_SubnetId",
+                table: "Houses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubnetId",
+                table: "Houses",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Houses_Subnets_SubnetId",
+                table: "Houses",
+                column: "SubnetId",
+                principalTable: "Subnets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
