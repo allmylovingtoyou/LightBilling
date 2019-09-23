1cc2f2ce304f9edbdf2098dcddc9b127999bc3b2
terekhov-aa <12root12@gmail.com>
2019-09-23 23:20:07 +0300

    init network tools

---
+++ NetworkUtils/DhcpConfigGenerator.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Linq;
+using Db;
+using Microsoft.EntityFrameworkCore;
+using static System.Console;
+
+namespace NetworkUtils
+{
+    public class DhcpConfigGenerator
+    {
+        public void Generate()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var clients = db.Clients
+                    .Include(c => c.House)
+                    .Where(c => c.GreyAddress != null)
+                    .Where(c => c.House != null)
+                    .Where(c => c.House.Subnet != null);
+
+                var grouped = clients.GroupBy(x => x.House.Subnet);
+
+                foreach (var pair in grouped)
+                {
+                    var net = pair.Key;
+                    WriteLine($"subnet {net.Net} {MapNetMask(net.Mask)}");
+                    WriteLine("{");
+                    WriteLine($"    option routers {net.Gateway};");
+                    WriteLine($"    option subnet-mask {MapNetMask(net.Mask)};");
+
+                    foreach (var cl in pair.Select(x => x)
+                        .Where(x => !string.IsNullOrWhiteSpace(x.MacAddress))
+                        .Where(x => !string.IsNullOrWhiteSpace(x.GreyAddress.Address))
+                    )
+                    {
+                        Write($"    host GEN_{cl.Login} ");
+                        Write("{ ");
+                        Write($"hardware ethernet {cl.MacAddress}; fixed-address {cl.GreyAddress.Address};");
+                        Write("}");
+                        WriteLine();
+                    }
+                    WriteLine("}");
+                    WriteLine();
+                }
+            }
+        }
+
+        public string MapNetMask(int mask)
+        {
+            if (mask == 27)
+            {
+                return "255.255.255.224";
+            }
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ NetworkUtils/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NetworkUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var dhcp = new DhcpConfigGenerator();
+            dhcp.Generate();
+            
+            
+            Console.WriteLine("End");
+            Console.WriteLine();
+        }
+    }
+}
